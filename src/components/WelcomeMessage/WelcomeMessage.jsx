e118ea355f8fe89ad84de976a9b020fd23fb0e58
Hadar Nimtsovich <hadarnimtso@gmail.com>
2021-03-14 13:59:23 +0200

    Add welcome message

---
+++ src/components/WelcomeMessage/WelcomeMessage.jsx
@@ -0,0 +1,9 @@
+import styles from "./WelcomMessage.module.css";
+
+const WelcomeMessage = () => (
+  <div className={styles.WelcomeMessage}>
+    <h1>Welcome to THE Calculator!</h1>
+  </div>
+);
+
+export default WelcomeMessage;
